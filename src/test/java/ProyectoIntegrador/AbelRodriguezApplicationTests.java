8aa6f374f938316546ad912145f222e0f36ac0fa
Abel <arodriguez@inti.gob.ar>
2025-07-16 16:17:05 -0300

    Reapply "Proyecto final"
    This reverts commit 20257a1b9f215da22002e0ca40f40eeb9730cc35.

---
+++ src/test/java/ProyectoIntegrador/AbelRodriguezApplicationTests.java
@@ -0,0 +1,13 @@
+package ProyectoIntegrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbelRodriguezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
